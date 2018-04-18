cb1d9ae58aefceafe45c8c92647849438d58ab5b
Raymond Zhang <rui.zhang@walmartlabs.com>
2018-04-18 10:43:16 -0700

    add jar file

---
+++ src/main/java/rui/test/concord/start/App.java
@@ -0,0 +1,13 @@
+package rui.test.concord.start;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
